51ff6c47bbea0e944dfb822dec9f91e565239250
Suteekshn Manchanda <suteekshnman@gmail.com>
2026-08-17 22:11:15 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int marks[5]={44,55,66,7,77};
+    int sz= sizeof(marks);
+    
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[4]<<endl;
+    cout<<sz<<endl;
+    return 0;
+}
