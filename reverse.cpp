cb1e55ffcddd8a3c1f84735d48390fb3d6ea64ab
Suteekshn Manchanda <suteekshnman@gmail.com>
2026-08-18 07:04:33 +0530

    Refactor reverse logic to use for loop

--- reverse.cpp
+++ reverse.cpp
@@ -8,7 +8,7 @@ int main() {
     int start = 0;
     int end = size - 1;
 
-    while(start < end) {
+    for(int i = 0; i < size / 2; i++) {
         swap(arr[start], arr[end]);
 
         start++;
