a7fb31683d9080b15974ebef83c6336f35978378
Suteekshn Manchanda <suteekshnman@gmail.com>
2026-08-18 07:29:28 +0530

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+    vector<int> v={1, 2, 3, 4, 5};
+    cout<<"Vector elements are: ";
+    for(int i=0;i<v.size();i++) {
+        cout<<v[i]<<" ";
+
+    }
+    
+}
