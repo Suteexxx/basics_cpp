72efe91e0eb27838ad6f311e1219aa2731b13df3
Suteekshn Manchanda <suteekshnman@gmail.com>
2026-08-19 07:24:42 +0530

    Add files via upload

---
+++ vecfun.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+    vector<int> vec;
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    cout<<vec.size()<<endl;
+    vec.pop_back();
+    cout<<vec.size()<<endl;
+    vec.front()=100;
+    cout<<vec.front()<<endl;
+    return 0;
+}
